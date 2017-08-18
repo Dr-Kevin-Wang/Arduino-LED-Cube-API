b6db7c6e8562f6ea2fbef866f636373cf7eff772
Dr-Kevin-Wang <wangyunxiao11@yahoo.com>
2017-08-18 13:11:51 -0700

    Add comments in PHP API

--- Server/API.php
+++ Server/API.php
@@ -1,20 +1,23 @@
 <?php
 	session_start();
-	
+	//Connect to db
 	$user = "root";
-	$pass = "UnitedPolaris";
+	$pass = "pw";
 	$pdo = new PDO('mysql:host=localhost;dbname=cube', $user, $pass);
 	
+	//Read from POST request
 	$x = $_POST["x"];
 	$y = $_POST["y"];
 	$z = $_POST["z"];
 	$state = $_POST["state"];
+	//Write to DB
 	$stmt = $pdo->prepare("
 		UPDATE onoff
 		SET onoff = '$state'
 		WHERE x = '$x' AND y = '$y' AND z = '$z'");
 	$stmt->execute();
 	
+	//Write to TXT file
 	$file = fopen("arduino.txt", "w");
 	$txt = "$x$y$z$state\n";
 	fwrite($file, $txt);
